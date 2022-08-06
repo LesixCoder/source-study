ec366fb37f555043472823e61c4b9d3857404d7a
liusixin <fordreamxkhl@163.com>
2022-08-06 18:10:23 +0800

    build: Create rush-prettier autoinstaller

--- assets/scripts/index.js
+++
@@ -1,9 +0,0 @@
-console.log('index init');
-// console.log(Vue);
-var app6 = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
-  }
-});
-// export default app6;
--- src/recursive.js
+++
@@ -1,7 +0,0 @@
-function tail(i) {
-  if(i > 3) return i
-  console.log('修改前', i);
-
-  return arguments.callee(i + 1)
-}
-tail(0)
